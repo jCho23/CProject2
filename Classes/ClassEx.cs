67f9c4cb952c9faca5c1c165382db49325c14902
June Cho <junecho.ucb@gmail.com>
2017-09-14 18:30:12 -0700

    Created Method and objects

--- Classes/ClassEx.cs
+++ Classes/ClassEx.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 namespace CProject2.Classes
 {
-    public class ClassEx
+    public class Person
     {
-        public ClassEx()
+        public string Name;
+
+        public void Introduce(string to)
         {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+    }
+
+    class ClassEx
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "John";
+            person.Introduce("June");
         }
     }
 }
