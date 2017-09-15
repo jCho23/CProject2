60e6ee4fdd85f3fcf7bfd35c4bd6f23dd68fb057
June Cho <junecho.ucb@gmail.com>
2017-09-14 18:57:22 -0700

    CleanUp

--- Constructors/Constructors.cs
+++ Constructors/Constructors.cs
@@ -5,15 +5,28 @@
 
 namespace CProject2.Constructors
 {
-    //This is how to Declare a Constructor
-    //The Constructor is the same name as the class (*REQUIREMENT*)
-    public class Constructors
+    ////Demo 1
+    ////This is how to Declare a Constructor
+    ////The Constructor is the same name as the class (*REQUIREMENT*)
+    //public class Constructors
+    //{
+    //    //This is the Constructor
+    //    //Constuctors, unlike other Methods, does not have a return type
+    //    //If you need to initialize an Object upon creation, use a constuctor 
+    //    public Constructors()
+    //    {
+    //    }
+    //}
+
+    //Demo 2
+    public class Customer
     {
-        //This is the Constructor
-        //Constuctors, unlike other Methods, does not have a return type
-        //If you need to initialize an Object upon creation, use a constuctor 
-        public Constructors()
+        public string Name;
+
+        //Here
+        public Customer(string name)
         {
+            this.Name = name;
         }
     }
 }
