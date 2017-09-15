c0aaee5b5550f7d76efb75cedb3e700da7d43c5d
June Cho <junecho.ucb@gmail.com>
2017-09-14 20:31:01 -0700

    CleanUp

--- Constructors/ConstructorsEx.cs
+++ Constructors/ConstructorsEx.cs
@@ -9,6 +9,11 @@ namespace CProject2.Constructors
 		//*NOTE= This is not the best way to declare fields (i.e. public)
 		public int Id;
 		public string Name;
+
+		//Declaring a constructor to intialize this field upon instanciation of a customer 
+        //Two ways to declare a Constructor
+        //
+
 	}
 
     public class ConstructorsEx
@@ -20,7 +25,6 @@ namespace CProject2.Constructors
             Console.WriteLine(customers.Id);
             Console.WriteLine(customers.Name);
 
-            //Declaring 
         }
     }
 }
