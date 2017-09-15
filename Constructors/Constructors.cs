c9cfd9ee50abc5c5e3ded05772304e52d2fe63f7
June Cho <junecho.ucb@gmail.com>
2017-09-14 18:51:46 -0700

    Declaring C's

--- Constructors/Constructors.cs
+++ Constructors/Constructors.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+
+//Constructor = A Method that is called when an instance of a class is created
+//Why do we need a Constructor? = To put an object in an early state
+
 namespace CProject2.Constructors
 {
+    //This is how to Declare a Constructor
+    //The Constructor is the same name as the class (
     public class Constructors
     {
         public Constructors()
