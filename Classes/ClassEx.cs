51971de8e9c97a87e0b16a9bb095fc3c8513da0b
June Cho <junecho.ucb@gmail.com>
2017-09-14 18:33:05 -0700

    Parse

--- Classes/ClassEx.cs
+++ Classes/ClassEx.cs
@@ -9,6 +9,14 @@ namespace CProject2.Classes
         {
             Console.WriteLine("Hi {0}, I am {1}", to, Name);
         }
+
+        public Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
     }
 
     class ClassEx
@@ -18,6 +26,11 @@ namespace CProject2.Classes
             var person = new Person();
             person.Name = "John";
             person.Introduce("June");
+
+            //Creating a Person Object from a String
+            //We are going to use a Parse Method
+
+
         }
     }
 }
