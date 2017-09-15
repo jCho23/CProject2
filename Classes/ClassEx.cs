c2716658eb03d76c3838a5eff0c4a6268154ca3c
June Cho <junecho.ucb@gmail.com>
2017-09-14 18:39:05 -0700

    CleanUp

--- Classes/ClassEx.cs
+++ Classes/ClassEx.cs
@@ -1,43 +1,43 @@
-﻿using System;
-namespace CProject2.Classes
-{
-    public class Person
-    {
-        public string Name;
+﻿//using System;
+//namespace CProject2.Classes
+//{
+//    public class Person
+//    {
+//        public string Name;
 
-        public void Introduce(string to)
-        {
-            Console.WriteLine("Hi {0}, I am {1}", to, Name);
-        }
+//        public void Introduce(string to)
+//        {
+//            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+//        }
 
-        //Defining Parse Method here
-        //This is an Instance Method
-        //We Changed this to static so we don't have to declare it twice down below
-        public static Person Parse(string str)
-        {
-            //Here we are creating a person object
-            var person = new Person();
-            //Creating a name field
-            person.Name = str;
+//        //Defining Parse Method here
+//        //This is an Instance Method
+//        //We Changed this to static so we don't have to declare it twice down below
+//        public static Person Parse(string str)
+//        {
+//            //Here we are creating a person object
+//            var person = new Person();
+//            //Creating a name field
+//            person.Name = str;
 
-            //Returning that person object
-            return person;
-        }
-    }
+//            //Returning that person object
+//            return person;
+//        }
+//    }
 
-    class ClassEx
-    {
-        static void Main(string[] args)
-        {
-            //var person = new Person();
-            var person = Person.Parse("John");
-            //person.Name = "John";
-            person.Introduce("June");
+//    class ClassEx
+//    {
+//        static void Main(string[] args)
+//        {
+//            //var person = new Person();
+//            var person = Person.Parse("John");
+//            //person.Name = "John";
+//            person.Introduce("June");
 
-            //Creating a Person Object from a String
-            //We are going to use a Parse Method
+//            //Creating a Person Object from a String
+//            //We are going to use a Parse Method
 
 
-        }
-    }
-}
+//        }
+//    }
+//}
