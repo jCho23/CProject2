0804a2cb1b46eb9533016d9128138349f013bb78
June Cho <junecho.ucb@gmail.com>
2017-09-15 19:09:40 -0700

    TwoWays

--- Constructors/ConstructorsEx.cs
+++ Constructors/ConstructorsEx.cs
@@ -3,19 +3,24 @@ using CProject2.Constructors;
 
 namespace CProject2.Constructors
 {
-	//Creating a Customer Class
-	public class Customers
-	{
-		//*NOTE= This is not the best way to declare fields (i.e. public)
-		public int Id;
-		public string Name;
+    //Creating a Customer Class
+    public class Customers
+    {
+        //*NOTE= This is not the best way to declare fields (i.e. public)
+        public int Id;
+        public string Name;
 
-		//Declaring a constructor to intialize this field upon instanciation of a customer 
+        //Declaring a constructor to intialize this field upon instanciation of a customer 
         //Two ways to declare a Constructor
-        //
+        //1) public Customer{}
+        public Customers(int id)
+        {
 
-	}
+        }
+    }
 
+
+}
     public class ConstructorsEx
     {
        static void Main(string[] args)
