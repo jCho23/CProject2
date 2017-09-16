d7b8bf90127ad61444877e45ac8ad30ce35cdd64
June Cho <junecho.ucb@gmail.com>
2017-09-15 21:54:31 -0700

    Points

--- Methods/MethodsEx.cs
+++ Methods/MethodsEx.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 namespace CProject2.Methods
 {
+    //Here, Point represents a point on the screen so you need 2 fields-- X and Y
+    public class Point
+    {
+        //Points
+        public int X;
+        public int Y;
+
+
+    }
+
+
+
     public class MethodsEx
     {
-        public MethodsEx()
+        static void Main(string[] args)
         {
+            
         }
     }
 }
