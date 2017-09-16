0afac1aba389b703667c07483d455548eb4128a7
June Cho <junecho.ucb@gmail.com>
2017-09-15 19:50:21 -0700

    OI notes

--- ObjectInitializers/ObjectInitializer.cs
+++ ObjectInitializers/ObjectInitializer.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+
+//ObjectInitializer= A syntax for quickly initialising an object without the need to call one of its constructors
+//Why do we need? = To avoid creating multiple Constructors
+
 namespace CProject2.ObjectInitializers
 {
     public class ObjectInitializer
