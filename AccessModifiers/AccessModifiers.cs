c50aac89cee21678ee0d18721c231257c636daf7
June Cho <junecho.ucb@gmail.com>
2017-09-17 11:13:21 -0700

    FieldsAndClasses

--- AccessModifiers/AccessModifiers.cs
+++ AccessModifiers/AccessModifiers.cs
@@ -6,20 +6,24 @@
 ////Encapsulation = Hide details in Fields or Methods  
 //// 1) Define field as private
 //// 2) Provide getter/ setter methods as public 
-
+//// 
+////*Classes and Methods= PascalCase
+/// *Fields= camelCase prefixed with an underline 
 public class Person
 {
-    private string Name;
+    //Notice that the fields are represented by _name
+    private string _name;
 
+    //Here, with a Setter and Getter, we can introduce Logic into our fields
     public void SetName(string name)
     {
         if (!String.IsNullOrEmpty(name))
-            this.Name = name;
+            this._name = name;
     }
 
     public string GetName()
     {
-        return Name;
+        return _name;
     }
 }
 
