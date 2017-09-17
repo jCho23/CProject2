96acd347b56bbeaf8f9504993f27fd462544bf44
June Cho <junecho.ucb@gmail.com>
2017-09-17 11:20:27 -0700

    Private

--- AccessModifiers/AccessModifiersEx.cs
+++ AccessModifiers/AccessModifiersEx.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 namespace CProject2.AccessModifiers
 {
+    public class Person
+    {
+        //Notice that this is a private field illustrated by "_birthdate"
+        private DateTime _birthdate;    
+    }
+
     public class AccessModifiersEx
     {
-        public AccessModifiersEx()
+        static void Main(string[] args)
         {
+            
         }
     }
 }
