a86b34a86e38f9dfe4538d347b6ebde619e98840
June Cho <junecho.ucb@gmail.com>
2017-09-16 21:11:29 -0700

    SecondOverload

--- Methods/MethodsEx.cs
+++ Methods/MethodsEx.cs
@@ -46,7 +46,9 @@ namespace CProject2.Methods
             point.Move(new Point(40, 60));
             Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
 
-
+            //Second Overload
+            point.Move(100, 200);
+            Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
         }
     }
 }
