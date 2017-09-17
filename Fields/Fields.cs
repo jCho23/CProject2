a68037fdd2554f776e761210818955d7ebf4fd75
June Cho <junecho.ucb@gmail.com>
2017-09-17 10:11:09 -0700

    Initialization

--- Fields/Fields.cs
+++ Fields/Fields.cs
@@ -1,6 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 ////Initialization
+//Here, we are only using the constructor when we need to initialize fields,
+//based on the values passed from the outside
+public class Cutomer
+{
+    //Notice that we can directly Initialize the Orders Fields,
+    //without the need of a constructor
+    //Also, no matter how many constructors we have, the Orders Field
+    //will ALWAYS be initialized to an empty list
+    List<Order> Orders = new List<Order>();
+}
 
 
 
