8116664e0ab6e67324f514e3d8e0bcddf6665929
June Cho <junecho.ucb@gmail.com>
2017-09-17 10:21:40 -0700

    Customer Class

--- Fields/FieldsEx.cs
+++ Fields/FieldsEx.cs
@@ -1,6 +1,19 @@
 ﻿using System;
+
 namespace CProject2.Fields
 {
+    //Customer Class
+    public class Customer
+    {
+        //Here, we are declaring Fields
+        public int Id;
+        public string Name;
+    }
+
+    //Order Class
+
+
+
     public class FieldsEx
     {
         static void Main(string[] args)
