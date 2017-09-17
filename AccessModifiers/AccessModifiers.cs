2f38ffd100ddc2659adcb9775790609d9a575a98
June Cho <junecho.ucb@gmail.com>
2017-09-17 11:03:57 -0700

    Encapsulation

--- AccessModifiers/AccessModifiers.cs
+++ AccessModifiers/AccessModifiers.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 
+////Access Modifiers= A way to control access to a class and / or its members
+////Why do we need? To create saftey nets in our program
+//// Encapsulation =   
 //Public 
 
-//Ptivate
+//Private
 
 //Protected
 
