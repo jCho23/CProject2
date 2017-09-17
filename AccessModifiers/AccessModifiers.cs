8e9ffbc694567e8795bb51aaf382e93708f927b5
June Cho <junecho.ucb@gmail.com>
2017-09-17 10:46:14 -0700

    AccessModifiers Types

---
+++ AccessModifiers/AccessModifiers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//Public 
+
+//Ptivate
+
+//Protected
+
+//Internal
+
+//Protected Internal
+
+
+namespace CProject2.AccessModifiers
+{
+    public class AccessModifiers
+    {
+        public AccessModifiers()
+        {
+        }
+    }
+}
