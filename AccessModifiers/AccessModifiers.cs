40be3c42fbbdd366a878e14472d7fb712e650ecf
June Cho <junecho.ucb@gmail.com>
2017-09-17 11:08:35 -0700

    Demo Encapsulation

--- AccessModifiers/AccessModifiers.cs
+++ AccessModifiers/AccessModifiers.cs
@@ -2,7 +2,28 @@
 
 ////Access Modifiers= A way to control access to a class and / or its members
 ////Why do we need? To create saftey nets in our program
-//// Encapsulation =   
+
+////Encapsulation = Hide details in Fields or Methods  
+//// 1) Define field as private
+//// 2) Provide getter/ setter methods as public 
+
+public class Person
+{
+    private string Name;
+
+    public void SetName(string name)
+    {
+        if (!String.IsNullOrEmpty(name))
+            this.Name = name;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+}
+
+
 //Public 
 
 //Private
