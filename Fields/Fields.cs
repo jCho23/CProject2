35a7b252cf2b37dc8003499cc47ef4e9d603861a
June Cho <junecho.ucb@gmail.com>
2017-09-16 22:44:05 -0700

    Initialization

---
+++ Fields/Fields.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+////Initialization
+
+
+
+
+////Read-Only Fields
+
+
+namespace CProject2.Fields
+{
+    public class Fields
+    {
+        public Fields()
+        {
+        }
+    }
+}
