de6f83a2ff75a772fa5ecca10b0431c13063d142
June Cho <junecho.ucb@gmail.com>
2017-09-18 20:16:05 -0700

    Created Fields Example

--- PropertiesInClasses/PropertiesInClasses.cs
+++ PropertiesInClasses/PropertiesInClasses.cs
@@ -2,6 +2,22 @@
 
 ////Properties= A class member that encapsulates a getter/setter for accessing a field
 //// Why do we need Properties? = To create a getter/setter with less code
+ 
+////Fields Example
+public class Person
+{
+    private DateTime _birthdate;
+
+    public void SetBirthdate(DateTime birthdate)
+    {
+        this._birthdate = birthdate;
+    }
+
+    public DateTime GetBirthdate()
+    {
+        return _birthdate;
+    }
+}
 
 namespace CProject2
 {
