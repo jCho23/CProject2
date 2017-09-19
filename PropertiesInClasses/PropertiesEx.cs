1689fa4972ef6257106e9d40329aeb49797f28f4
June Cho <junecho.ucb@gmail.com>
2017-09-19 09:04:37 -0700

    Auto-Implemented property Example

--- PropertiesInClasses/PropertiesEx.cs
+++ PropertiesInClasses/PropertiesEx.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+
 namespace CProject2.PropertiesInClasses
 {
+    public class Person
+    {
+        //Here we are creating an Auto-Implemented property without any fields
+        //Note, that the compiler will create internally create a private field 
+        //And implements the "get" and "set" Accessor Methods
+        public DateTime Birthdate { get; set; }
+    }
+
     public class PropertiesEx
     {
-        public PropertiesEx()
+        static void Main(string[] args)
         {
+            
         }
     }
 }
